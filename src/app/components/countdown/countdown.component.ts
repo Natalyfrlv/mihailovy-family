e97dcb48fe33b7c9ad9e7b7e5eb2da0f06d6ee98
n.frolova <n.frolova@telecom-oil.ru>
2026-02-10 14:40:40 +0500

    countdown

--- src/app/components/countdown/countdown.component.ts
+++ src/app/components/countdown/countdown.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CountdownService } from '../../service/Countdown.service';
 @Component({
   selector: 'app-countdown',
   standalone: true,
@@ -7,6 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './countdown.component.html',
   styleUrl: './countdown.component.scss'
 })
-export class CountdownComponent {
+export class CountdownComponent implements OnInit {
+  targetDate: Date = new Date('2026-03-19T12:00:00');
+  timeLeft: any;
+  constructor(private countdown: CountdownService) {}
 
+  ngOnInit() {
+    this.timeLeft = this.countdown.getTimeLeft(this.targetDate);
+    setInterval(() => {
+      this.timeLeft = this.countdown.getTimeLeft(this.targetDate);
+    }, 1000);
+  }
 }
---
+++ src/app/service/countdown.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core'; // <- обязательно
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountdownService {
+  getTimeLeft(target: Date) {
+    const diff = target.getTime() - Date.now();
+
+    return {
+      days: Math.floor(diff / 1000 / 60 / 60 / 24),
+      hours: Math.floor(diff / 1000 / 60 / 60) % 24,
+      minutes: Math.floor(diff / 1000 / 60) % 60,
+      seconds: Math.floor(diff / 1000) % 60,
+    };
+  }
+}
